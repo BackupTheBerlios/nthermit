95d1d9dc647db2758df14d7eecadbfbf321c9ecc
jjhop <jjhop>
2005-06-13 23:54:23 +0000

    Rewizja poczatkowa. Zoastal nakreslony ogolny ksztalt testow, ale testy nie zostaly jeszcze zaimplementowane.

---
+++ NThermit.Test/RefreshCmdTest.cs
@@ -0,0 +1,55 @@
+// $Id: RefreshCmdTest.cs,v 1.1 2005/06/13 23:54:23 jjhop Exp $
+// NThermit - Visual Studio .NET 2003 add-in that makes easier cooperation with NAnt.
+
+// Copyright (C) 2005 - Konrad 'meydey' Madej <meydey@users.berlios.de>
+//                    - Rafal 'jjhop' Kotusiewicz <jjhop@users.berlios.de>
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+using System;
+using NUnit.Framework;
+using NThermit;
+
+namespace NThermit 
+{
+	/// <summary>
+	/// Summary description for RefreshCmdTest.
+	/// </summary>
+	[TestFixture]
+	public class RefreshCmdTest
+	{
+		[TestFixtureSetUp]
+		public void SetUp() 
+		{
+		}
+
+		[TestFixtureTearDown]
+		public void TearDown() 
+		{
+		}
+
+		[Test]
+		public void PrepareTest() 
+		{
+			throw new NotImplementedException("TestCase must be implemented.");
+		}
+
+		[Test]
+		public void ExecuteTest() 
+		{
+			throw new NotImplementedException("TestCase must be implemented.");
+		}
+	}
+}
